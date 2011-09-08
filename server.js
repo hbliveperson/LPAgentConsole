3adacc4b36cd62c3df3376c3cd07a9da91600aee
hbliveperson <hbrigman@liveperson.com>
2011-09-08 15:25:49 +0000

    Hello World

---
+++ server.js
@@ -0,0 +1,9 @@
+// Load the http module to create an http server.
+var http = require('http');
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(process.env.C9_PORT, '0.0.0.0');
+
+console.log('Server running.');
