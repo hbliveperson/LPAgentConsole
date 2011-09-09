29ed1956f1ef1bd42b65dac29e29b6bec7d9aedb
hbliveperson <hbrigman@liveperson.com>
2011-09-09 01:41:32 +0000

    request/response success

--- console.js
+++ console.js
@@ -1,17 +1,22 @@
 console.log('console.js is here');
 
-$.ajax({
-    cache: false,
-    dataType: 'json',
-    url: 'http://lpagentconsole.hbliveperson.cloud9ide.com/',
-    data: {
-        username: 'name',
-        password: 'pass'
-    },
-    error: function ajaxError(jqXHR, textStatus, errorThrown) {
-        console.log(textStatus);
-    },
-    success: function ajaxSuccess(data, textStatus, jqXHR) {
-        console.log(data);
-    }
+$(document).ready(function(){
+    $('#loginSubmit').click(function formSubmit(){
+        $.ajax({
+            cache: false,
+            dataType: 'json',
+            type: 'POST',
+            url: 'http://lpagentconsole.hbliveperson.cloud9ide.com/',
+            data: JSON.stringify({
+                user: $('#loginUser').val(),
+                pass: $('#loginPass').val()
+            }),
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus);
+            },
+            success: function ajaxSuccess(data, textStatus, jqXHR) {
+                console.log(data);
+            }
+        });
+    });
 });
--- server.js
+++ server.js
@@ -3,13 +3,27 @@ PORT = process.env.C9_PORT;
 
 // Load the http module to create an http server.
 var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+var util = require('util');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*'
+    var data = '';
+    
+    request.addListener('data',function(chunk) {
+        console.log('data:' + chunk);
+        data += chunk;
     });
-    response.end(JSON.stringify({status: 'success'}));
+    request.addListener('end',function() {
+        console.log('end: ' + data);
+        console.log(JSON.parse(data));
+        response.writeHead(200, {
+            'Content-Type': 'text/plain',
+            'Access-Control-Allow-Origin': '*'
+        });
+        response.write(data);
+        response.end('\n');
+    });
 }).listen(PORT, HOST);
 
 console.log('Server running on ' + HOST + ' on port ' + PORT);
